e75662ba54e64bd966c0d0d0bc61840ace7e305b
Dharma Teja Yalavarthi <dharmatejayalavarthi@Dharmas-MacBook-Air.local>
2021-12-18 11:52:04 -0500

    intialcommit

--- core/src/main/java/com/mysite/core/servlets/FormServlet.java
+++ core/src/main/java/com/mysite/core/servlets/FormServlet.java
@@ -1,15 +1,23 @@
 package com.mysite.core.servlets;
 
 import acscommons.com.jcraft.jsch.Logger;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.ServletResolverConstants;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.Node;
+import javax.jcr.Session;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
@@ -17,6 +25,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 
 @Component(immediate = true, service = Servlet.class, property = {
@@ -27,26 +36,49 @@ import java.io.IOException;
 })
 public class FormServlet extends SlingAllMethodsServlet {
     public static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(FormServlet.class);
+    ResourceResolver resourceResolver;
+    @Reference
+    ResourceResolverFactory resourceResolverFactory;
+
+
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
-    String height = request.getParameter("height");
-    String weight = request.getParameter("weight");
-    try{
-        if (height.matches("[0-9]+")&&weight.matches("[0-9]+")) {
-            double bmi = calculateBMI(
-                    Double.parseDouble(weight),
-                    Double.parseDouble(height));
-            response.getWriter().print("This is test servlet -" + height + "location -" + weight + "=bmi=" + bmi); //print the response
-        }
-        else{
-            LOGGER.error("please provide valid input");
-        }
-    } catch (Exception e){
-        LOGGER.error("error in form.");
-    }
-}
-private Double calculateBMI(Double weight, Double height){
-    return weight/ (height* height);
-}
-}
+        String height = request.getParameter("height");
+        String weight = request.getParameter("weight");
+        try {
+            if (height.matches("[0-9]+") && weight.matches("[0-9]+")) {
+                double bmi = calculateBMI(
+                        Double.parseDouble(weight),
+                        Double.parseDouble(height));
+                resourceResolver= getServiceUser(resourceResolverFactory);
+                String path = "/content/mysite/us/en";
+                Session session= resourceResolver.adaptTo(Session.class);
+                if(session!=null) {
+                    Node node = session.getNode(path);
+                    node.addNode("Dharma");
+                   /* node.setProperty("weight",weight);
+                    node.setProperty("height",height);*/
+                }
+                session.save();
 
+                response.getWriter().print("This is test servlet -" + height + "location -" + weight + "=bmi=" + bmi); //print the response
+            } else {
+                LOGGER.error("please provide valid input");
+            }
+        } catch (Exception e) {
+            LOGGER.error("error in form.");
+        }
+    }
+
+    private Double calculateBMI(Double weight, Double height) {
+        return weight / (height * height);
+    }
+
+    public  ResourceResolver getServiceUser(ResourceResolverFactory resourceResolverFactory) throws LoginException {
+        HashMap<String, Object> param = new HashMap<String, Object>();
+        param.put(ResourceResolverFactory.SUBSERVICE, "getResourceResolver");
+        resourceResolver = resourceResolverFactory.getServiceResourceResolver(param);
+        return resourceResolver;
+    }
+
+}
